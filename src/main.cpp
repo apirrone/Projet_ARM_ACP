2806a89771901bd02cc08ab4f742eabb13056768
antoine <awanegenebayo@gmail.com>
2017-09-10 16:41:01 +0200

    Added usage function

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include "PGM3D_Holder.h"
+
+using namespace std;
+
+void usage(char* programName){
+  cerr << "USAGE : " << programName <<" <file.pgm3d>" << endl;
+}
 
 int main(int argc, char *argv[]) {
 
+  if(argc != 2){
+    usage(argv[0]);
+    return EXIT_FAILURE;
+  }
+  
   PGM3D_Holder test = PGM3D_Holder(argv[1]); 
   
   return EXIT_SUCCESS;
